0ab8e34e33388ff12025c4a05969a891a9dcb8c4
Andre Erdmann <dywi@mailerd.de>
2012-05-30 18:08:30 +0200

    roverlay, fileio: refactored and @classmethod decorators removed

--- roverlay/fileio.py
+++ roverlay/fileio.py
@@ -15,217 +15,13 @@ from roverlay import tmpconst as const
 class DescriptionReader:
 	"""Description Reader"""
 
-	@classmethod
 	def __init__ ( self ):
 		"""Initializes a DESCRIPTION file reader."""
 		pass
 
-	@classmethod
-	def _get_fields_with_flag ( self, flag, foce_update=False ):
-
-		matching_fields = []
-
-		field = None
-		for field in const.DESCRIPTION_FIELD_MAP.keys():
-			if flag is None:
-				matching_fields.append ( field )
-
-			elif 'flags' in const.DESCRIPTION_FIELD_MAP [field]:
-				if flag in const.DESCRIPTION_FIELD_MAP [field] ['flags']:
-					matching_fields.append ( field )
-
-		del field
-		return matching_fields
+	# --- end of __init__ (...) ---
 
 
-	@classmethod
-	def _find_field ( self , field_identifier ):
-		"""Determines the real name of a field.
-
-		arguments:
-		* field_identifier -- name of the field as it appears in the DESCRIPTION file
-
-		At first, it is checked whether field_identifier matches the name of
-		a field listed in DESCRIPTION_FIELD_MAP (any match results in immediate return).
-		Then, a new iteration over the field map compares field_identifier
-		with all aliases until the first case-(in)sensitive match (-> immediate return).
-		None will be returned if none of the above searches succeed.
-
-		In other words: this method decides whether a field_identifier will be used and if so,
-		with which name.
-		"""
-
-		# save some time by prevent searching if field_id is empty
-		if not field_identifier:
-			return None
-
-		# search for real field names first
-		for field in const.DESCRIPTION_FIELD_MAP.keys():
-			if field_identifier == field:
-				return field
-
-		field_id_lower = field_identifier.lower()
-
-		for field in const.DESCRIPTION_FIELD_MAP.keys():
-
-			# does extra information (-> alias(es)) for this field exist?
-			if 'alias' in const.DESCRIPTION_FIELD_MAP [field]:
-
-				if 'withcase' in const.DESCRIPTION_FIELD_MAP [field] ['alias']:
-					for alias in const.DESCRIPTION_FIELD_MAP [field] ['alias'] ['withcase']:
-						if field_identifier == alias:
-							return field
-
-				if 'nocase' in const.DESCRIPTION_FIELD_MAP [field] ['alias']:
-					for alias in const.DESCRIPTION_FIELD_MAP [field] ['alias'] ['nocase']:
-						if field_id_lower == alias.lower():
-							return field
-
-				#if 'other_alias_type' in const.DESCRIPTION_FIELD_MAP [field] ['alias']:
-
-		# returning None if no valid field identifier matches
-		return None
-
-	@classmethod
-	def _make_values ( self, value_str, field_context=None ):
-		"""Extracts relevant data from value_str and returns them as list.
-
-		arguments:
-		* value_str -- string that represents the (just read) values
-		* field_context -- field name the value belongs to; optional, defaults to None
-
-		It's useful to set field_context 'cause several fields ('Depends') have
-		multiple values arranged in a list (dep0, dep1 [, depK]*).
-		"""
-
-		svalue_str = value_str.strip()
-
-		if not svalue_str:
-			# empty value(s)
-			return []
-
-		elif field_context is None:
-			# default return if no context given
-			return [ svalue_str ]
-
-		elif self._check_fieldflag ( field_context ):
-			# value str is not empty and have flags for field_context, check these
-
-			if self._check_fieldflag ( field_context, 'isList' ):
-					# split up this list (that is separated by commata and/or semicolons)
-					return re.split (const.DESCRIPTION_LIST_SPLIT_REGEX, svalue_str, 0)
-
-			elif self._check_fieldflag ( field_context, 'isWhitespaceList' ):
-					# split up this list (that is separated whitespace)
-					return re.split ( '\s+', svalue_str, 0 )
-
-
-		# default return
-		return [ svalue_str ]
-
-	@classmethod
-	def _check_fieldflag ( self, field, flag_to_check=None ):
-		"""Checks if the given field has the specified flag and returns a bool.
-
-		arguments:
-		* field -- name of the field that should be checked
-		* flag_to_check -- name of the flag to check; optional, defaults to None
-
-		This method acts as 'field has any flags?' if flag_to_check is None (its default value).
-		"""
-
-		if field in const.DESCRIPTION_FIELD_MAP:
-			if 'flags' in const.DESCRIPTION_FIELD_MAP [field]:
-				if flag_to_check in const.DESCRIPTION_FIELD_MAP [field] ['flags']:
-					return True
-				elif flag_to_check is None:
-					# 'flags' exist, return true
-					return True
-
-		return False
-
-	@staticmethod
-	def _get_desc_from_file ( filepath, pkg_name='.' ):
-		"""Reads a file returns the description data.
-
-		arguments:
-		* filepath -- file to read (str; path to tarball or file)
-		* pkg_name -- name of the package, in tarballs the description file
-		              is located in <pkg_name>/ and thus this argument is required.
-		              Defaults to '.', set to None to disable.
-
-		All exceptions are passed to the caller (TarError, IOErr, <custom>).
-		<filepath> can either be a tarball in which case the real DESCRIPTION
-		file is read (<pkg_name>/DESCRIPTION) or a normal file.
-		"""
-
-		logging.write ( "Starting to read file '" + str ( filepath ) + "' ...\n" )
-
-		if not ( isinstance ( filepath, str ) and filepath ):
-			raise Exception ( "bad usage" )
-
-		# read describes how to import the lines from a file (e.g. rstrip())
-		#  fh, th are file/tar handles
-		read = th = fh = None
-
-		if tarfile.is_tarfile ( filepath ):
-			# filepath is a tarball, open tar handle + file handle
-			th = tarfile.open ( filepath, 'r' )
-			if pkg_name:
-				fh = th.extractfile ( os.path.join ( pkg_name, const.DESCRIPTION_FILE_NAME ) )
-			else:
-				fh = th.extractfile ( const.DESCRIPTION_FILE_NAME )
-
-			# have to decode the lines
-			read = lambda lines : [ line.decode().rstrip() for line in lines ]
-		else:
-			# open file handle only
-			fh = open ( filepath, 'r' )
-			read = lambda lines : [ line.rstrip() for line in lines ]
-
-		x = None
-		read_lines = read ( fh.readlines() )
-		del x, read
-
-		fh.close()
-		if not th is None: th.close()
-		del fh, th
-
-		return read_lines
-
-	@staticmethod
-	def _get_fileinfo ( filepath ):
-		"""Returns some info about the given filepath as dict whose contents are
-			the file path, the file name ([as package_file with suffix and]
-			as filename with tarball suffix removed), the package name
-			and the package_version.
-
-		arguments:
-		* filepath --
-		"""
-
-		package_file = os.path.basename ( filepath )
-
-		filename = re.sub ( const.RPACKAGE_SUFFIX_REGEX + '$', '', package_file )
-
-		# todo move that separator to const
-		package_name, sepa, package_version = filename.partition ( '_' )
-
-		if not sepa:
-			# file name unexpected, tarball extraction will (probably) fail
-			#raise Exception ("file name unexpected")
-			logging.write ( "unexpected file name '" + filename + "'.\n" )
-
-		return dict (
-			filepath        = filepath,
-			filename        = filename,
-			package_file    = package_file,
-			package_name    = package_name,
-			#package_origin = ?,
-			package_version = package_version,
-		)
-
-	@classmethod
 	def _parse_read_data ( self, read_data ):
 		"""Verifies and parses/fixes read data.
 
@@ -233,16 +29,25 @@ class DescriptionReader:
 		* read_data -- data from file, will be modified
 		"""
 
-		def stats ( data ):
-			"""Temporary function that prints some info about the given data."""
-			field = None
-			logging.write ( "=== this is the list of read data ===\n" )
-			for field in read_data.keys():
-				logging.write ( field + " = " + str ( read_data [field] ) + "\n" )
-			logging.write ( "=== end of list ===\n" )
-			del field
+		def get_fields_with_flag ( flag, foce_update=False ):
 
-		def _value_in_strlist ( _val, _list, case_insensitive=True ):
+			matching_fields = []
+
+			field = None
+			for field in const.DESCRIPTION_FIELD_MAP.keys():
+				if flag is None:
+					matching_fields.append ( field )
+
+				elif 'flags' in const.DESCRIPTION_FIELD_MAP [field]:
+					if flag in const.DESCRIPTION_FIELD_MAP [field] ['flags']:
+						matching_fields.append ( field )
+
+			del field
+			return matching_fields
+
+		# --- end of get_fields_with_flag (...) ---
+
+		def value_in_strlist ( _val, _list, case_insensitive=True ):
 			"""Returns true if value is in the given list."""
 			el = None
 			if case_insensitive:
@@ -258,9 +63,7 @@ class DescriptionReader:
 
 			del el
 			return False
-
-
-		stats ( read_data )
+		# --- end of value_in_strlist (...) ---
 
 		field = None
 
@@ -270,14 +73,14 @@ class DescriptionReader:
 				read_data [field] = const.DESCRIPTION_FIELD_MAP [field] ['default_value']
 
 		# join values to a single string
-		for field in self._get_fields_with_flag ( 'joinValues' ):
+		for field in get_fields_with_flag ( 'joinValues' ):
 			if field in read_data.keys():
 				read_data [field] = ' ' . join ( read_data [field] )
 
 		# ensure that all mandatory fields are set
 		missing_fields = list()
 
-		for field in self._get_fields_with_flag ( 'mandatory' ):
+		for field in get_fields_with_flag ( 'mandatory' ):
 			if field in read_data:
 				if not len (read_data [field]):
 					missing_fields.append ( field )
@@ -294,14 +97,9 @@ class DescriptionReader:
 			# skip _fileinfo
 			if field  != '_fileinfo':
 				if 'allowed_values' in const.DESCRIPTION_FIELD_MAP [field]:
-					if not _value_in_strlist ( read_data [field],
+					if not value_in_strlist ( read_data [field],
 						const.DESCRIPTION_FIELD_MAP [field] ['allowed_values']
 					): unsuitable_fields.append ( field )
-
-
-		stats ( read_data )
-
-
 
 		valid = True
 
@@ -328,8 +126,8 @@ class DescriptionReader:
 		del field
 
 		return valid
+	# --- end of _parse_read_data (...) ---
 
-	@classmethod
 	def readfile ( self, filepath ):
 		"""Reads a DESCRIPTION file and returns the read data if successful, else None.
 
@@ -347,15 +145,205 @@ class DescriptionReader:
 		e.g. if OS_TYPE is not unix).
 		"""
 
+		def get_fileinfo ( filepath ):
+			"""Returns some info about the given filepath as dict whose contents are
+				the file path, the file name ([as package_file with suffix and]
+				as filename with tarball suffix removed), the package name
+				and the package_version.
+
+			arguments:
+			* filepath --
+			"""
+
+			package_file = os.path.basename ( filepath )
+
+			filename = re.sub ( const.RPACKAGE_SUFFIX_REGEX + '$', '', package_file )
+
+			# todo move that separator to const
+			package_name, sepa, package_version = filename.partition ( '_' )
+
+			if not sepa:
+				# file name unexpected, tarball extraction will (probably) fail
+				#raise Exception ("file name unexpected")
+				logging.write ( "unexpected file name '" + filename + "'.\n" )
+
+			return dict (
+				filepath        = filepath,
+				filename        = filename,
+				package_file    = package_file,
+				package_name    = package_name,
+				#package_origin = ?,
+				package_version = package_version,
+			)
+
+		# --- end of get_fileinfo (...) ---
+
+
+		def make_values ( value_str, field_context=None ):
+			"""Extracts relevant data from value_str and returns them as list.
+
+			arguments:
+			* value_str -- string that represents the (just read) values
+			* field_context -- field name the value belongs to; optional, defaults to None
+
+			It's useful to set field_context 'cause several fields ('Depends') have
+			multiple values arranged in a list (dep0, dep1 [, depK]*).
+			"""
+
+			def check_fieldflag ( field, flag_to_check=None ):
+				"""Checks if the given field has the specified flag and returns a bool.
+
+				arguments:
+				* field -- name of the field that should be checked
+				* flag_to_check -- name of the flag to check; optional, defaults to None
+
+				This method acts as 'field has any flags?' if flag_to_check is None (its default value).
+				"""
+
+				if field in const.DESCRIPTION_FIELD_MAP:
+					if 'flags' in const.DESCRIPTION_FIELD_MAP [field]:
+						if flag_to_check in const.DESCRIPTION_FIELD_MAP [field] ['flags']:
+							return True
+						elif flag_to_check is None:
+							# 'flags' exist, return true
+							return True
+
+				return False
+			# --- end of check_fieldflag (...) ---
+
+			svalue_str = value_str.strip()
+
+			if not svalue_str:
+				# empty value(s)
+				return []
+
+			elif field_context is None:
+				# default return if no context given
+				return [ svalue_str ]
+
+			elif check_fieldflag ( field_context ):
+				# value str is not empty and have flags for field_context, check these
+
+				if check_fieldflag ( field_context, 'isList' ):
+						# split up this list (that is separated by commata and/or semicolons)
+						return re.split (const.DESCRIPTION_LIST_SPLIT_REGEX, svalue_str, 0)
+
+				elif check_fieldflag ( field_context, 'isWhitespaceList' ):
+						# split up this list (that is separated whitespace)
+						return re.split ( '\s+', svalue_str, 0 )
+
+
+			# default return
+			return [ svalue_str ]
+
+		# --- end of make_values (...) ---
+
+		def get_desc_from_file ( filepath, pkg_name='.' ):
+			"""Reads a file returns the description data.
+
+			arguments:
+			* filepath -- file to read (str; path to tarball or file)
+			* pkg_name -- name of the package, in tarballs the description file
+							  is located in <pkg_name>/ and thus this argument is required.
+							  Defaults to '.', set to None to disable.
+
+			All exceptions are passed to the caller (TarError, IOErr, <custom>).
+			<filepath> can either be a tarball in which case the real DESCRIPTION
+			file is read (<pkg_name>/DESCRIPTION) or a normal file.
+			"""
+
+			logging.write ( "Starting to read file '" + str ( filepath ) + "' ...\n" )
+
+			if not ( isinstance ( filepath, str ) and filepath ):
+				raise Exception ( "bad usage" )
+
+			# read describes how to import the lines from a file (e.g. rstrip())
+			#  fh, th are file/tar handles
+			read = th = fh = None
+
+			if tarfile.is_tarfile ( filepath ):
+				# filepath is a tarball, open tar handle + file handle
+				th = tarfile.open ( filepath, 'r' )
+				if pkg_name:
+					fh = th.extractfile ( os.path.join ( pkg_name, const.DESCRIPTION_FILE_NAME ) )
+				else:
+					fh = th.extractfile ( const.DESCRIPTION_FILE_NAME )
+
+				# have to decode the lines
+				read = lambda lines : [ line.decode().rstrip() for line in lines ]
+			else:
+				# open file handle only
+				fh = open ( filepath, 'r' )
+				read = lambda lines : [ line.rstrip() for line in lines ]
+
+			x = None
+			read_lines = read ( fh.readlines() )
+			del x, read
+
+			fh.close()
+			if not th is None: th.close()
+			del fh, th
+
+			return read_lines
+
+		# --- end of get_desc_from_file (...) ---
+
+		def find_field ( field_identifier ):
+			"""Determines the real name of a field.
+
+			arguments:
+			* field_identifier -- name of the field as it appears in the DESCRIPTION file
+
+			At first, it is checked whether field_identifier matches the name of
+			a field listed in DESCRIPTION_FIELD_MAP (any match results in immediate return).
+			Then, a new iteration over the field map compares field_identifier
+			with all aliases until the first case-(in)sensitive match (-> immediate return).
+			None will be returned if none of the above searches succeed.
+
+			In other words: this method decides whether a field_identifier will be used and if so,
+			with which name.
+			"""
+
+			# save some time by prevent searching if field_id is empty
+			if not field_identifier:
+				return None
+
+			# search for real field names first
+			for field in const.DESCRIPTION_FIELD_MAP.keys():
+				if field_identifier == field:
+					return field
+
+			field_id_lower = field_identifier.lower()
+
+			for field in const.DESCRIPTION_FIELD_MAP.keys():
+
+				# does extra information (-> alias(es)) for this field exist?
+				if 'alias' in const.DESCRIPTION_FIELD_MAP [field]:
+
+					if 'withcase' in const.DESCRIPTION_FIELD_MAP [field] ['alias']:
+						for alias in const.DESCRIPTION_FIELD_MAP [field] ['alias'] ['withcase']:
+							if field_identifier == alias:
+								return field
+
+					if 'nocase' in const.DESCRIPTION_FIELD_MAP [field] ['alias']:
+						for alias in const.DESCRIPTION_FIELD_MAP [field] ['alias'] ['nocase']:
+							if field_id_lower == alias.lower():
+								return field
+
+					#if 'other_alias_type' in const.DESCRIPTION_FIELD_MAP [field] ['alias']:
+
+			# returning None if no valid field identifier matches
+			return None
+
+		# --- end of find_field (...) ---
+
+
 		read_data = dict ()
-		fileinfo = DescriptionReader._get_fileinfo ( filepath )
+		fileinfo = get_fileinfo ( filepath )
 
 
 		try:
-			desc_lines = DescriptionReader._get_desc_from_file (
-				filepath, fileinfo ['package_name']
-			)
-
+			desc_lines = get_desc_from_file ( filepath, fileinfo ['package_name'] )
 
 		except IOError as err:
 			# <todo>
@@ -378,7 +366,7 @@ class DescriptionReader:
 				if field_context:
 					# context is set => append values
 
-					for val in self._make_values ( sline, field_context ):
+					for val in make_values ( sline, field_context ):
 						read_data [field_context] . append ( val )
 				else:
 					# no valid context => ignore line
@@ -392,7 +380,7 @@ class DescriptionReader:
 
 				if line_components [1]:
 					# line contains a field separator, set field context
-					field_context = self._find_field ( line_components [0] )
+					field_context = find_field ( line_components [0] )
 
 					if field_context:
 						# create a new empty list for field_context
@@ -400,7 +388,7 @@ class DescriptionReader:
 
 						# add values to read_data
 						#  no need to check line_components [2] 'cause [1] was a true str
-						for val in self._make_values ( line_components [2], field_context ):
+						for val in make_values ( line_components [2], field_context ):
 							read_data [field_context] . append ( val )
 
 					else:
@@ -424,8 +412,8 @@ class DescriptionReader:
 
 
 		if self._parse_read_data ( read_data ):
-			logging.write ( '## success ##\n' )
-			logging.write ( ( str ( read_data ) ) )
+			#logging.write ( '## success ##\n' )
+			#logging.write ( ( str ( read_data ) ) )
 			return dict (
 				fileinfo = fileinfo,
 				description_data = read_data
@@ -434,3 +422,4 @@ class DescriptionReader:
 			logging.write ( '## fail ##\n' )
 			return None
 
+	# --- end of readfile (...) ---
