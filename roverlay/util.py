50ba18a562ef365ffbd2c7ba49a9027ba101234b
André Erdmann <dywi@mailerd.de>
2012-06-14 20:52:32 +0200

    extend roverlay.util
    * added keepenv that selectively imports os.environ
    * added misc functions
    
    	modified:   roverlay/util.py

--- roverlay/util.py
+++ roverlay/util.py
@@ -6,6 +6,8 @@ import re
 import os.path
 import logging
 
+import os
+
 from roverlay import config
 
 LOGGER = logging.getLogger ( 'util' )
@@ -46,3 +48,56 @@ def get_packageinfo ( filepath ):
 	)
 
 # --- end of get_packageinfo (...) ---
+
+def get_extra_packageinfo ( package_info, name ):
+	return dict (
+		PKG_DISTDIR = os.path.dirname ( package_info ['package_file'] ),
+		EBUILD_FILE = os.path.join (
+			config.get_or_fail ( [ 'OVERLAY', 'dir' ] ),
+			config.get_or_fail ( [ 'OVERLAY', 'category' ] ),
+			package_info [ 'ebuild_filename'].partition ( '-' ) [0],
+			package_info [ 'ebuild_filename'] + ".ebuild"
+		)
+	) [name]
+# --- end of get_extra_packageinfo (...) ---
+
+def pipe_lines ( _pipe, use_filter=False, filter_func=None ):
+	lines = _pipe.decode().split ('\n')
+	if use_filter:
+		return filter ( filter_func, lines )
+	else:
+		return lines
+# --- end of pipe_lines (...) ---
+
+
+def keepenv ( *to_keep, local_env=None ):
+	if local_env is None:
+		myenv = dict()
+	else:
+		myenv = local_env
+
+	for item in to_keep:
+		if isinstance ( item, tuple ) and len ( item ) == 2:
+
+			var      = item [0]
+			fallback = item [1]
+		else:
+			var      = item
+			fallback = None
+
+		if isinstance ( var, str ):
+			if var in os.environ:
+				myenv [var] = os.environ [var]
+			elif not fallback is None:
+				myenv [var] = fallback
+		else:
+			varlist = var
+			for var in varlist:
+				if var in os.environ:
+					myenv [var] = os.environ [var]
+				elif not fallback is None:
+					myenv [var] = fallback
+
+	# -- for
+	return myenv if local_env is None else None
+# --- end of keepenv (...) ---
