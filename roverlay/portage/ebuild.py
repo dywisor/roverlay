d4019f10979a2d10c2fb85ae5a5bf02cd44a07a2
André Erdmann <dywi@mailerd.de>
2012-06-15 22:29:37 +0200

    Ebuild.py: fixes and cut long DESCRIPTION values
    	modified:   roverlay/portage/ebuild.py

--- roverlay/portage/ebuild.py
+++ roverlay/portage/ebuild.py
@@ -6,6 +6,8 @@ import copy
 
 import roverlay.config
 
+from roverlay.util import shorten_str
+
 
 class Ebuild ( object ):
 	EBUILD_INDENT = roverlay.config.get ( 'EBUILD.indent', '\t' )
@@ -168,10 +170,12 @@ class Ebuild ( object ):
 
 	def suggest_dir_name ( self ):
 		"""Suggests a direcory name for this Ebuild."""
-		if 'pkg_name' in self._data:
+		if self._data is None:
+			return self._ebuild_name.partition ( '-' ) [0]
+		elif 'pkg_name' in self._data:
 			return self._data ['pkg_name']
 		else:
-			return self.suggest_name().partition ( '-' )
+			return self.suggest_name().partition ( '-' ) [0]
 	# --- end of suggest_dir_name (...) ---
 
 	def suggest_name ( self, fallback_name='' ):
@@ -253,23 +257,30 @@ class Ebuild ( object ):
 
 		# --- end of get_dep_and_use () ---
 
-		def make_var ( varname, value=None, oneline_list=True, indent_list=True, indent_level=0 ):
+		def make_var (
+			varname,
+			value=None, oneline_list=True, indent_list=True, indent_level=0
+		):
 			"""Creates a <name>=<value> statement for ebuilds.
 
 			arguments:
-			* varname -- name of the variable
-			* value -- value of the variable. This has to be either None (the default),
-			           str, or list of str.
-			* oneline_list -- if value is a list: controls whether its components should be
-			                  put into one line (True) or multiple (False). Defaults to True.
-			* indent_list -- if value is a list and not oneline_list:
-			                 controls whether each value line should be indentend
-			                 (by indent_level + 1) or not ("by 0"). Defaults to True.
+			* varname      -- name of the variable
+			* value        -- value of the variable.
+			                   This has to be either None (the default), str,
+			                   or list of str.
+			* oneline_list -- if value is a list: controls whether its components
+			                   should be put into one line (True) or multiple.
+			                   Defaults to True.
+			* indent_list  -- if value is a list and not oneline_list:
+			                   controls whether each value line should be
+			                   indentend (by indent_level + 1) or not ("by 0").
+			                   Defaults to True.
 			* indent_level -- current indentation level, defaults to 0
 
 			"""
 
-			# assumption: value is either None, scalar with str representation or list of str
+			# assumption: value is either None,
+			#              scalar with str representation or list of str
 			var_value = None
 
 			if not value:
@@ -288,16 +299,37 @@ class Ebuild ( object ):
 			else:
 				var_value = str ( value )
 
-			return indent_level * Ebuild.EBUILD_INDENT + varname + '="' + var_value + '"'
+
+			# (TODO)
+			# fixing ebuild var values here
+
+			# cut DESCRIPTION line if too long
+			if varname == 'DESCRIPTION':
+				var_value = shorten_str ( var_value, 45, '... (see metadata)' )
+
+
+			ret ='%s%s="%s"' % (
+				indent_level * Ebuild.EBUILD_INDENT,
+				varname,
+				var_value
+			)
+
+			# (TODO)
+			# fixing ebuild var lines here
+
+			return ret
 
 		# --- end of make_var (...) ---
 
 		def remove_newlines ( line_list ):
-			"""
-			Removes leading, ending and repeated empty/whitespace lines in line_list.
+			"""Removes leading, ending and repeated blank lines in line_list.
 
 			arguments:
-			* line_list
+			* line_list --
+
+			returns: filtered lines
+
+			TODO: check if a filter function could be used for this
 			"""
 			lines = []
 			line = None
@@ -325,26 +357,38 @@ class Ebuild ( object ):
 
 		# --- end of remove_newlines (...) ---
 
-		def add_easyvar ( ebuild_content, varname, value_key=None, add_newline=False):
-			"""Adds a 'simple' variable to the ebuild lines. This means that it
-			can directly be taken from self._data [value_key]. This method assumes
-			that value_key exists in self._data, any exceptions (KeyError) will be passed.
+		def add_easyvar (
+			ebuild_content, varname,
+			value_key=None, add_newline=False
+		):
+			"""Adds a 'simple' variable to the ebuild lines.
+			This means that it can directly be taken from self._data [value_key].
+			This method assumes that value_key exists in self._data,
+			any exceptions (KeyError) will be passed.
 
 			arguments:
-			* ebuild_content -- list of ebuild text lines, will be modified directly,
-			                     so copy it before calling addvar if you need the original list.
-			* varname -- name of the variable. Nothing happens if this is None.
-			* value_key -- key of the value, defaults to varname if it is None
-			* add_newline -- adds a newline after the var statement, defaults to False
+			* ebuild_content -- list of ebuild text lines, will be modified
+			                     directly, so copy it before calling addvar if
+			                     you need the original list.
+			* varname        -- name of the variable.
+			                     Nothing happens if this is None.
+			* value_key      -- key of the value,
+			                     defaults to varname if it is None
+			* add_newline    -- adds a newline after the var statement,
+			                     defaults to False
 
-			Returns given list (ebuild_content), which will then have been modified.
+			returns: given+modified list (ebuild_content)
 			"""
 
 			if not varname is None:
 				if value_key is None:
-					ebuild_content.append ( make_var ( varname, self._data [varname] ) )
+					ebuild_content.append (
+						make_var ( varname, self._data [varname] )
+					)
 				else:
-					ebuild_content.append ( make_var ( varname, self._data [value_key] ) )
+					ebuild_content.append (
+						make_var ( varname, self._data [value_key] )
+					)
 
 				if add_newline:
 					ebuild_content.append ( "" )
@@ -353,6 +397,7 @@ class Ebuild ( object ):
 
 		# --- end of add_easyvar (...) ---
 
+		# -- actual start of _make_ebuild_lines (...) --
 		try:
 			ebuild_lines = []
 
@@ -366,42 +411,46 @@ class Ebuild ( object ):
 
 			ebuild_lines.append ( "" )
 
+			# TODO/FIXME: this makes DESCRIPTION mandatory, maybe check with
+			#  >if 'DESCRIPTION' in self._data<
 			add_easyvar ( ebuild_lines, "DESCRIPTION" )
 
-			if 'SRC_URI' in self._data:
-				add_easyvar ( ebuild_lines, "SRC_URI" )
-			else:
-				# > calculate SRC_URI using self._data ['origin'] -- either here or in eclass
-				ebuild_lines.append ( make_var ( "SRC_URI" , None ) )
-				# (temporary, todo) setting restrict to fetch
-				ebuild_lines.append ( make_var ( "RESTRICT" , "fetch" ) )
+			add_easyvar ( ebuild_lines, "SRC_URI", add_newline=True )
 
-			ebuild_lines.append ( "" )
-
-			# LICENSE ?
+			# FIXME/TODO: LICENSE?
 
 			dep_and_use = get_dep_and_use ()
 
-			# check that IUSE has more than one element, don't write IUSE="${IUSE:-}" etc.
+			# check that IUSE has more than one element,
+			#  don't write IUSE="${IUSE:-}" etc.
 			if len ( dep_and_use ['IUSE'] ) > 1:
-				ebuild_lines.append ( make_var ( "IUSE", dep_and_use ['IUSE'], True ) )
+				ebuild_lines.append (
+					make_var ( "IUSE", dep_and_use ['IUSE'], True )
+				)
 
 			if 'R_SUGGESTS' in dep_and_use:
-				ebuild_lines.append ( make_var ( "R_SUGGESTS", dep_and_use ['R_SUGGESTS'], False ) )
+				ebuild_lines.append (
+					make_var ( "R_SUGGESTS", dep_and_use ['R_SUGGESTS'], False )
+				)
 
 			# see IUSE
 			if len ( dep_and_use ['DEPEND'] ) > 1:
-				ebuild_lines.append ( make_var ( "DEPEND", dep_and_use ['DEPEND'], False ) )
+				ebuild_lines.append (
+					make_var ( "DEPEND", dep_and_use ['DEPEND'], False )
+				)
 
 			# see IUSE
 			if len ( dep_and_use ['RDEPEND'] ) > 1:
-				ebuild_lines.append ( make_var ( "RDEPEND", dep_and_use ['RDEPEND'], False ) )
+				ebuild_lines.append (
+					make_var ( "RDEPEND", dep_and_use ['RDEPEND'], False )
+				)
 
 			del dep_and_use
 			return remove_newlines ( ebuild_lines )
 
 		except ( ValueError, KeyError, NameError ) as err:
-			self.logger.error ( "Cannot create ebuild text lines. The error message was %s.", str ( err ) )
+			self.logger.exception ( err )
+			self.logger.error ( "Cannot create ebuild text lines." )
 			return None
 
 		# --- end of make_ebuild_lines (...) ---
