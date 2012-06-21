ceb4dbb9c99d05dc191acf2182e29ef3d3d55038
André Erdmann <dywi@mailerd.de>
2012-06-21 18:53:02 +0200

    replaced run_overlaycreation test script
    * run_overlaycreation uses OverlayCreator now,
       usage is ./run_overlaycreation [--show] [--write] [--help] <R packages>
    
    	modified:   run_overlaycreation.py

--- run_overlaycreation.py
+++ run_overlaycreation.py
@@ -1,82 +1,47 @@
 #!/usr/bin/env python3
+from sys import exit
+from sys import argv as ARGV
 
-import os.path
-import sys
-import logging
-
-import roverlay.static.depres
-
-from roverlay                 import config
-from roverlay.packageinfo     import PackageInfo
-from roverlay.ebuild.creation import EbuildCreation
-from roverlay.overlay         import Overlay
-from roverlay.depres          import simpledeprule, listeners
-
-# -- prepare
-
-WRITE_OVERLAY = False
-if len ( sys.argv ) > 1:
-	if sys.argv [1] == '--write':
-		WRITE_OVERLAY = True
+if True in ( x in ARGV for x in ( '--help', '-h', '?' ) ):
+	print ( '\n'.join ( (
+		"usage: %s <pkgs|args>" % ARGV[0],
+		"* pkgs ::= [pkg]*",
+		"* pkg  ::= <path to package file, relative or absolute",
+		"* args ::= [arg]*",
+		"* arg  ::= --write|--show|--help",
+		"** write ^= write overlay (somewhere in /tmp by default)",
+		"** show  ^= print overlay to stdout",
+	) ) )
+	exit ( 0 )
 
 
-# the package used for testing
-seewave_f = os.path.abspath ( 'examples/packages/seewave_1.6.3.tar.gz' )
-# the overlay dir
-overlay_d = "/tmp/R-Overlay_0"
-config.access().inject ( "OVERLAY.dir", overlay_d )
+from roverlay.overlay.creator import OverlayCreator
 
-# dep resolver setup
-resolver = roverlay.static.depres.resolver()
-# log everything
-resolver.set_logmask ( -1 )
+default_pkg = ( 'examples/packages/seewave_1.6.3.tar.gz', )
+SHOW  = False
+WRITE = False
 
-# add simple rules to the resolver
-rulepool = simpledeprule.SimpleDependencyRulePool (
-	'test pool',
-	filepath=os.path.abspath ( 'simple-deprules.conf' ),
-	priority=25
-)
+pkg_list = list()
+for i, x in enumerate ( ARGV ):
+	if i == 0:
+		pass
+	elif x == '--show':
+		SHOW = True
+	elif x == '--write':
+		WRITE = True
+	else:
+		pkg_list.append ( x )
 
-resolver.add_rulepool ( rulepool )
-
-
-# eclass files
-eclass_list = ( 'R-packages', )
-eclass_files = tuple ( os.path.abspath ( os.path.join ( 'eclass', "%s.eclass" % x ) ) for x in eclass_list )
-
-#for e in eclass_files:
-#	if not os.path.isfile ( e ):
-#		raise Exception ( "eclass %s is missing!" % e )
+if len (pkg_list) == 0: pkg_list = default_pkg
 
 
-o = Overlay (
-	name="R-packages",
-	logger=None,
-	directory=None,
-	default_category="sci-R",
-	eclass_files=eclass_files
-)
+o = OverlayCreator()
 
-p = PackageInfo ( filepath=seewave_f )
-e = EbuildCreation ( p )
+o.add_package_files ( *pkg_list )
+o.can_write_overlay = WRITE
+o.run()
+if SHOW: o.show_overlay()
+o.close ( write=True )
 
-# -- run
-
-e.run()
-
-resolver.close()
-
-if p ['ebuild'] is None:
-	sys.stderr.write ( "No ebuild created!\n" )
-else:
-	try:
-		o.add ( p )
-		o.show()
-		if WRITE_OVERLAY:
-			o.write()
-			sys.stderr.write ( "Overlay written - directory is '%s'.\n" % overlay_d )
-	except Exception as e:
-		sys.stderr.write ( "Overlay creation/update failed!\n" )
-		raise
-
+if not ( WRITE or SHOW ):
+	print ( "Use '--show' or '--write' if you want ebuild/metadata/Manifest output." )
