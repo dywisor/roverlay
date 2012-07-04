d598dd5307b8ed3cb076ca3e97f7493f64ef1da6
André Erdmann <dywi@mailerd.de>
2012-07-04 20:19:35 +0200

    main script
    * added some options
    * unique exit codes
    
    	modified:   main.py

--- main.py
+++ main.py
@@ -2,23 +2,49 @@
 
 import sys
 
-# other roverlay modules will be imported later
-import roverlay
+# roverlay modules will be imported later
 
 HIDE_EXCEPTIONS = False
 
-def die ( msg=None, code=1 ):
-	if msg:
-		sys.stderr.write ( msg + "\n" )
-	sys.exit ( code )
+class DIE ( object ):
+	NOP          =   0
+	ERR          =   1
+	BAD_USAGE    =   3
+	ARG          =   9
+	CONFIG       =  10
+	OV_CREATE    =  20
+	SYNC         =  30
+	CMD_LEFTOVER =  90
+	IMPORT       =  91
+	UNKNOWN      =  95
+	INTERRUPT    = 130
+
+	@staticmethod
+	def die ( msg=None, code=None ):
+		code = DIE.ERR if code is None else code
+		if msg is not None:
+			sys.stderr.write ( msg + "\n" )
+#		else:
+#			sys.stderr.write ( "died." )
+		sys.exit ( code )
+	# --- end of die (...) ---
+
+# --- DIE: exit codes ---
+die = DIE.die
 
 
 if __name__ != '__main__':
-	die ( "Please don't import this script..." )
+	die ( "Please don't import this script...", DIE.BAD_USAGE )
 
 
 # get args
-import roverlay.argutil
+try:
+	import roverlay.argutil
+except ImportError:
+	if HIDE_EXCEPTIONS:
+		die ( "Cannot import roverlay modules!", DIE.IMPORT )
+	else:
+		raise
 
 COMMAND_DESCRIPTION = {
 	'sync'           : 'sync repos',
@@ -32,15 +58,27 @@ commands, config_file, additional_config, extra_opts = \
 		CMD_DESC=COMMAND_DESCRIPTION,
 		DEFAULT_CONFIG="R-overlay.conf"
 	)
+
+OPTION = extra_opts.get
+
 del roverlay.argutil
 
 # -- load config
+
+try:
+	import roverlay
+except ImportError:
+	if HIDE_EXCEPTIONS:
+		die ( "Cannot import roverlay modules!", DIE.IMPORT )
+	else:
+		raise
+
 try:
 	roverlay.load_config_file ( config_file, extraconf=additional_config )
 	del config_file, additional_config
 except:
 	if HIDE_EXCEPTIONS:
-		die ( "Cannot load config file %r." % config_file )
+		die ( "Cannot load config file %r." % config_file, DIE.CONFIG )
 	else:
 		raise
 
@@ -49,72 +87,120 @@ except:
 
 actions = set ( filter ( lambda x : x != 'nop', commands ) )
 
-if 'sync' in actions and extra_opts ['nosync']:
-	die ( "sync command blocked by --nosync opt." )
+if 'sync' in actions and OPTION ( 'nosync' ):
+	die ( "sync command blocked by --nosync opt.", DIE.ARG )
 
 del commands
 
 
 if not actions:
 	# this happens if a command is nop
-	die ( "Nothing to do!", 0 )
+	die ( "Nothing to do!", DIE.NOP )
 
 
 # -- import roverlay modules
 
-from roverlay.remote          import RepoList
-from roverlay.overlay.creator import OverlayCreator
+try:
+	from roverlay.remote          import RepoList
+	from roverlay.overlay.creator import OverlayCreator
+except ImportError:
+	if HIDE_EXCEPTIONS:
+		die ( "Cannot import roverlay modules!", DIE.IMPORT )
+	else:
+		raise
 
 
-# -- run
 
-actions_done = list()
+# -- run methods (and some vars)
 
-### sync / nosync
-# always run 'cause commands = {create,sync} and create implies (no)sync
+actions_done = set()
+set_action_done = actions_done.add
 
-# set up the repo list
-repo_list = RepoList ( sync_enabled=not extra_opts ['nosync'] )
+def optionally ( call, option, *args, **kw ):
+	if OPTION ( option ):
+		return call ( *args, **kw )
+# --- end of optionally (...) ---
 
-## extra_opts->distdir ... TODO
-repo_list.load()
-
-## this runs _nosync() or _sync(), depending on extra_opts->nosync
-repo_list.sync()
-
-if 'sync' in actions:
-	actions_done.append ( 'sync' )
-
-###
-
-# run overlay creation
-
-if 'create' in actions:
+#repo_list = None
+#overlay   = None
+def run_sync():
+	if "sync" in actions_done: return
 	try:
+		# set up the repo list
+		global repo_list
+		repo_list = RepoList (
+			sync_enabled   = not OPTION ( 'nosync' ),
+			force_distroot = OPTION ( 'force_distroot' )
+		)
+
+		## extra_opts->distdir ... TODO
+		if 'distdirs' in extra_opts:
+			repo_list.add_distdirs ( OPTION ( 'distdirs' ) )
+		else:
+			# default repo list
+			repo_list.load()
+
+		## this runs _nosync() or _sync(), depending on extra_opts->nosync
+		repo_list.sync()
+
+		set_action_done ( "sync" )
+
+	except KeyboardInterrupt:
+		die ( "Interrupted", DIE.INTERRUPT )
+	except:
+		if HIDE_EXCEPTIONS:
+				die (
+					"nosync() failed!" if OPTION ( "nosync" ) \
+						else "sync() failed!",
+					DIE.SYNC
+				)
+		else:
+			raise
+# --- end of run_sync() ---
+
+def run_overlay_create():
+	if "create" in actions_done: return
+	#run_sync()
+	try:
+		global overlay
 		overlay = OverlayCreator()
-		overlay.can_write_overlay = extra_opts ['write']
+		# explicitly allow overlay writing (FIXME: remove that in OverlayCreator)
+		overlay.can_write_overlay = OPTION ( 'write_overlay' )
 
 		repo_list.add_packages ( overlay.add_package )
 
-		overlay.run()
+		overlay.run ( close_when_done=True )
 
-		if extra_opts ['show']:
-			overlay.show_overlay()
+		optionally ( overlay.write_overlay, 'write_overlay' )
+		optionally ( overlay.show_overlay,  'show_overlay'  )
+		if OPTION ( 'print_stats' ): print ( "\n" + overlay.stats_str() )
 
-		if overlay.can_write_overlay:
-			overlay.write_overlay()
+		set_action_done ( "create" )
 
-		# write overlay on close
-		overlay.close()
-
-		actions_done.append ( 'create' )
-
+	except KeyboardInterrupt:
+		die ( "Interrupted", DIE.INTERRUPT )
 	except:
 		if HIDE_EXCEPTIONS:
-			die ( "Overlay creation failed.", 15 )
+			die ( "Overlay creation failed.", DIE.OV_CREATE )
 		else:
 			raise
+	finally:
+		if 'overlay' in locals() and not overlay.closed:
+			# This is important 'cause it unblocks remaining ebuild creation
+			# jobs/threads, specifically waiting EbuildJobChannels in depres.
+			# It also writes the deps_unresolved file
+			overlay.close()
+# --- end of run_overlay_create() ---
 
+# -- run
 
-if len ( actions ) != len ( actions_done ):
-	die ( "Some actions (out of %r) could not be performed!" % actions, 90 )
+# always run sync 'cause commands = {create,sync} and create implies (no)sync
+run_sync()
+
+if 'create' in actions: run_overlay_create()
+
+if len ( actions ) > len ( actions_done ):
+	die (
+		"Some actions (out of %r) could not be performed!" % actions,
+		DIE.CMD_LEFTOVER
+	)
