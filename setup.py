90f2ed47f9ff87a81b8b88f0b4abc57e1e674e3e
André Erdmann <dywi@mailerd.de>
2012-08-03 17:01:31 +0200

    exclude roverlay.py from setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,27 @@
 
 from distutils import core
 
+VERSION = '0.1'
+
 core.setup (
 	name         = 'R_Overlay',
-	version      = '0.1',
+	version      = VERSION,
 	description  = 'Automatically generated overlay of R packages (SoC2012)',
 	author       = 'André Erdmann',
 	author_email = 'dywi@mailerd.de',
 	license      = 'GPL-2', #?
 	url          = 'http://git.overlays.gentoo.org/gitweb/?p=proj/R_overlay.git;a=summary',
-#	py_modules   = ['roverlay'],
-	packages     = ( 'roverlay', ),
-	scripts      = ( 'roverlay.py', ),
+	packages     = (
+		'roverlay',
+		'roverlay/config',
+		'roverlay/depres',
+		'roverlay/depres/simpledeprule',
+		'roverlay/ebuild',
+		'roverlay/overlay',
+		'roverlay/overlay/manifest',
+		'roverlay/overlay/metadata',
+		'roverlay/recipe',
+		'roverlay/remote',
+		'roverlay/rpackage',
+	),
 )
