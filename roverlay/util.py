1b78546536ddad0c80eaa6c98d4e073f0c7c3357
André Erdmann <dywi@mailerd.de>
2013-02-05 18:26:31 +0100

    roverlay/util: for_all_files(), get_dict_hash()
    for_all_files(): accepts a list of "files or dirs" and calls a function
                     for each file (dirs will be recursively expanded)
    
    get_dict_hash(): a (slow) function that creates a hash for a dict
                     by hashing a frozenset of (key,value)-tuples

--- roverlay/util.py
+++ roverlay/util.py
@@ -6,12 +6,55 @@
 
 """provides utility functions commonly used"""
 
-__all__= [ 'dodir', 'keepenv', 'sysnop', 'get_dict_hash', 'priosort', ]
+__all__= [
+	'dodir', 'keepenv', 'sysnop', 'get_dict_hash', 'priosort',
+	'for_all_files'
+]
 
 import os
 import logging
 
 LOGGER = logging.getLogger ( 'util' )
+
+def for_all_files (
+	files_or_dirs, func,
+	args=(), kwargs={}, file_filter=None, ignore_missing=False
+):
+	"""
+	Runs "func ( <file>, *args, **kwargs )" for each <file> in files_or_dirs.
+	Dirs will be recursively "expanded" (= for all files/dirs in dir...).
+
+	arguments:
+	* files_or_dirs  -- an iterable with files or dirs
+	* func           -- function that will be called for each file
+	* args           -- args that will be passed to each func call
+	                     Defaults to () (empty tuple)
+	* kwargs         -- keyword args that will be passed to each func call
+	                     Defaults to {} (empty dict)
+	* file_filter    -- if not None: func will only be called if this function
+	                                 returns True for <file>
+	                     Defaults to None
+	* ignore_missing -- if True: do not raise an exception if a file/dir is
+	                             missing
+	                     Defaults to False
+	"""
+	# alternative: os.walk()
+	def recursive_do ( fpath ):
+		if os.path.isfile ( fpath ):
+			if file_filter is None or file_filter ( fpath ):
+				func ( fpath, *args, **kwargs )
+		elif os.path.isdir ( fpath ):
+			for fname in os.listdir ( fpath ):
+				recursive_do ( fpath + os.sep + fname )
+		elif os.access ( fpath, os.F_OK ):
+			raise Exception ( "{}: neither a file nor a dir.".format ( fpath ) )
+		elif not ignore_missing:
+			raise Exception ( "{!r} does not exist!".format ( fpath ) )
+	# --- end of recursive_do (...) ---
+
+	for f in files_or_dirs:
+		recursive_do ( f )
+# --- end of for_all_files (...) ---
 
 def priosort ( iterable ):
 	"""Sorts the items of an iterable by priority (lower value means higher
@@ -31,6 +74,17 @@ def priosort ( iterable ):
 
 	return sorted ( iterable, key=priokey )
 # --- end of priosort (...) ---
+
+def get_dict_hash ( kwargs ):
+	# dict is not hashable, instead hash a frozenset of (key,value) tuples
+	# !!! this operations costs (time)
+	return hash (
+		frozenset (
+			( k, v ) for k, v in kwargs.items()
+		)
+	)
+# --- end of get_dict_hash (...) ---
+
 
 def keepenv ( *to_keep ):
 	"""Selectively imports os.environ.
