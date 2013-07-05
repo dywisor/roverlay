5039d0929d5dbbc802d22908b32935bebaed0e08
André Erdmann <dywi@mailerd.de>
2013-07-05 18:45:58 +0200

    files/hooks/skel.sh: template for hook scripts

---
+++ files/hooks/skel.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# -*- coding: utf-8 -*-
+# <short description>
+# template for roverlay hooks
+#
+# <more informative description about what this script does>
+#
+set -u
+
+## load core functions
+. "${FUNCTIONS?}" || exit
+#dont_run_as_root
+
+## load helper functions
+#$lf ...
+
+# hook body starts here
+true
