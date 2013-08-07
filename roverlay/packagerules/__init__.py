b4c86bbcfc6f1278a43e76ac9ce29ffdb9b29096
André Erdmann <dywi@mailerd.de>
2013-08-07 17:57:30 +0200

    roverlay/packagerules: remove TODO note
    Remove old TODO note (everything listed there has implemented).

--- roverlay/packagerules/__init__.py
+++ roverlay/packagerules/__init__.py
@@ -3,9 +3,3 @@
 # Copyright (C) 2013 André Erdmann <dywi@mailerd.de>
 # Distributed under the terms of the GNU General Public License;
 # either version 2 of the License, or (at your option) any later version.
-
-# TODO:
-# * load rules from a file (make them accessible)
-# -> syntax
-# * logging
-# * doc/rst
