30112a08e243f51806fbe567ae6c23623aa070e5
André Erdmann <dywi@mailerd.de>
2013-08-09 16:58:10 +0200

    files/scripts/run-roverlay.sh: --help, cleanup

--- files/scripts/run-roverlay.sh
+++ files/scripts/run-roverlay.sh
@@ -7,7 +7,23 @@
 #
 # So, it's safe to set up a cronjob that calls this script.
 #
+# Dependencies:
+# * lockfile-progs (app-misc/lockfile-progs)
+# * portage for repoman
+# * [roverlay]
+#
 set -u
+
+# --- "config" ---
+
+# (TODO)
+REPOMAN_ARGS="--pretend full"
+
+ROVERLAY_ARGS="--strict"
+
+# --- end of config ---
+
+
 
 LC_COLLATE=C
 
@@ -15,6 +31,7 @@ LC_COLLATE=C
 
 readonly MY_LOCK=/run/lock/roverlay
 MY_LOCK_PID=
+
 
 # void run__cleanup ( **MY_LOCK_PID!, **MY_LOCK )
 #
@@ -32,11 +49,38 @@ run__cleanup() {
    lockfile-remove "${MY_LOCK}" || true
 }
 
+# void run_print_usage()
+#
+#  Prints the usage message.
+#
+run_print_usage() {
+echo "Usage: ${0##*/} [option...]
+
+options:
+   -h, --help        print this message and exit
+   +C, --no-create   disable overlay creation
+   +S, --no-sync     disable sync (offline mode)
+   +R, --no-repoman  disable repoman"
+}
+
+# @noreturn run_exit_usage ( [message], [code] )
+#
+#  Prints the usage message and exits afterwards.
+#  Calls die() for exiting if message or code are set.
+#
+run_exit_usage() {
+   if [ $# -eq 0 ]; then
+      print_usage
+      exit 0
+   else
+      print_usage 1>&2
+      echo 1>&2
+      die "$@"
+   fi
+}
+
 
 # prepare:
-
-# (TODO)
-REPOMAN_ARGS="--pretend full"
 
 WANT_ROVERLAY_CREATE=y
 WANT_ROVERLAY_SYNC=y
@@ -46,19 +90,18 @@ WANT_REPOMAN=y
 doshift=1
 while [ $# -gt 0 ]; do
    case "${1?}" in
-      '+C'|'--no-create') WANT_ROVERLAY_CREATE=n ;;
-      '+S'|'--no-sync') WANT_ROVERLAY_SYNC=n ;;
+      '-h'|'--help')       run_exit_usage ;;
+      '+C'|'--no-create')  WANT_ROVERLAY_CREATE=n ;;
+      '+S'|'--no-sync')    WANT_ROVERLAY_SYNC=n ;;
       '+R'|'--no-repoman') WANT_REPOMAN=n ;;
       *)
-         die "unknown arg: ${1}" ${EX_ARG_ERR?}
+         run_exit_usage "unknown arg: ${1}" ${EX_ARG_ERR?}
       ;;
    esac
    shift ${doshift} || OUT_OF_BOUNDS
    doshift=1
 done
 unset -v doshift
-
-# anything to do?
 
 # main:
 #
@@ -71,18 +114,21 @@ elif lockfile-create --retry 0 "${MY_LOCK}" 2>>${DEVNULL}; then
    # hold lock until done
    lockfile-touch "${MY_LOCK}" &
    MY_LOCK_PID="$!"
-   trap run__cleanup INT TERM EXIT
+   trap run__cleanup TERM EXIT
 
    roverlay_opts=""
    roverlay_opts() { roverlay_opts="${roverlay_opts-}${roverlay_opts:+ }$*"; }
 
-   ! yesno "${WANT_ROVERLAY_SYNC}" || roverlay_opts "--nosync"
+   ! yesno "${WANT_ROVERLAY_SYNC}" || roverlay_opts "--no-sync"
 
    # run roverlay
-   if ${ROVERLAY_EXE} ${roverlay_opts}; then
+   if ${ROVERLAY_EXE} ${ROVERLAY_ARGS-} ${roverlay_opts}; then
       # success, continue with repoman
       if yesno "${WANT_REPOMAN}"; then
-         ( cd "${S}" && repoman ${REPOMAN_ARGS} 2>&1 1>"${WORKDIR}/repoman.log"; )
+         (
+            cd "${S}" && \
+            repoman ${REPOMAN_ARGS-} 1>"${WORKDIR}/repoman.log" 2>&1
+         )
       fi
    fi
 else
