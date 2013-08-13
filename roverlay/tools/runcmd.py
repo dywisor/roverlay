3a58d432faa2cf5a9762dc73b14530fc3cad93fc
André Erdmann <dywi@mailerd.de>
2013-08-13 10:36:01 +0200

    roverlay/tools/runcmd: run command, get output

--- roverlay/tools/runcmd.py
+++ roverlay/tools/runcmd.py
@@ -12,22 +12,35 @@ import roverlay.strutil
 
 DEBUG_TO_CONSOLE = False
 
-def run_command ( cmdv, env, logger, return_success=False ):
-   if DEBUG_TO_CONSOLE:
-      cmd_call = subprocess.Popen ( cmdv, stdin=None, env=env )
-   else:
-      cmd_call = subprocess.Popen (
-         cmdv,
-         stdin=None,
-         stdout=subprocess.PIPE,
-         stderr=subprocess.PIPE,
-         env=env,
-      )
+def run_command_get_output (
+   cmdv, env, debug_to_console=False, use_filter=True, filter_func=None
+):
 
-   output = cmd_call.communicate()
+   # note that debug_to_console breaks calls that want to parse stdout
+   pipe_target = None if debug_to_console else subprocess.PIPE
+
+   cmd_call = subprocess.Popen (
+      cmdv, stdin=None, stdout=pipe_target, stderr=pipe_target, env=env
+   )
+   raw_output = cmd_call.communicate()
+
+   output = [
+      (
+         list ( roverlay.strutil.pipe_lines (
+            stream, use_filter=True, filter_func=None
+         ) )
+         if stream is not None else None
+      ) for stream in raw_output
+   ]
+
+   return ( cmd_call, output )
+# --- end of run_command_get_output (...) ---
+
+def run_command ( cmdv, env, logger, return_success=False ):
+   cmd_call, output = run_command_get_output ( cmdv, env, DEBUG_TO_CONSOLE )
 
    # log stderr
-   if output [1] and logger.isEnabledFor ( logging.WARNING ):
+   if output[1] and logger.isEnabledFor ( logging.WARNING ):
       for line in roverlay.strutil.pipe_lines ( output [1], use_filter=True ):
          logger.warning ( line )
 
