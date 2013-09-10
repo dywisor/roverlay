131b4c2e1326aa1415221be650357e6050691363
André Erdmann <dywi@mailerd.de>
2013-09-10 16:31:59 +0200

    roverlay/static: hookinfo
    constants for dealing with roverlay's hook scripts.

---
+++ roverlay/static/hookinfo.py
@@ -0,0 +1,31 @@
+# R overlay --
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013 André Erdmann <dywi@mailerd.de>
+# Distributed under the terms of the GNU General Public License;
+# either version 2 of the License, or (at your option) any later version.
+
+HOOK_DISABLE = ( False, -1, True, )
+
+_OVERLAY_SUCCESS = ( 'overlay_success', )
+
+# name -> (
+#    list|False|None events_to_enable_by_default,
+#    int|bool prio,
+#    bool is_hidden,
+#    ...
+# )
+#
+#  where prio is
+#  * None -> automatic assignment when creating links
+#  * <0   -> forbid hook usage
+#  * >=0  -> use hook with the given priority
+#
+HOOK_INFO = {
+   'skel'                  : HOOK_DISABLE,
+   'mux'                   : HOOK_DISABLE,
+   'create-metadata-cache' : ( _OVERLAY_SUCCESS, 50, False ),
+   'git-commit-overlay'    : ( _OVERLAY_SUCCESS, 80, False ),
+   'git-push'              : ( False, 85, False ),
+}
+
+get = HOOK_INFO.get
